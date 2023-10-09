d6f3a41525e0a242893b5b95188b2544f2b3a682
Danilo e Yan <retroville6@gmail.com>
2023-10-09 11:54:02 -0300

    Inicio do back end no log e sigin

--- ADM/model/Conexao.php
+++ ADM/model/Conexao.php
@@ -98,6 +98,50 @@
     }
     
 
+    public function validarUsuario($sql) {
+        $conexao = mysqli_connect($this->host, $this->user, $this->pass, $this->db);
+        
+        if (!$conexao) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        
+        $executar = mysqli_query($conexao, $sql);
+        
+        if (!$executar) {
+            die("Query failed: " . mysqli_error($conexao));
+        }
+        
+        session_start(); // Start the session
+        
+        if ($dados = mysqli_fetch_array($executar)) {
+            $_SESSION['ID'] = $dados['id'];
+            $_SESSION['NOME'] = $dados['nome'];
+            $_SESSION['EMAIL'] = $dados['email'];
+            $_SESSION['SENHA'] = $dados['senha'];
+            header('Location: ../../CLIENTE/view/logSigin.html');
+            
+            
+            // $_SESSION['user_data'][] = array('ID' => $dados['id'], 'EMAIL' => $dados['email'], 'SENHA' => $dados['senha']);
+        } else {
+            
+            session_destroy();
+            //header('Location: ../index.php');
+            ?>
+            <form action="../../CLIENTE/view/index.php" method="post" name="formMsg" id="formMsg">
+                <input type="hidden" name="msg">
+            </form>
+            <script>
+                var form = document.getElementById('formMsg');
+                form.submit();
+            </script>
+            <?php
+        }
+        
+        //var_dump($_SESSION['EMAIL']);
+        
+        mysqli_close($conexao);
+    }
+
 
     
 }
