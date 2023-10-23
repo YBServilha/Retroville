b02cf55e6fa8217058f0050c4418d6f2511c420f
Yan <retroville6@gmail.com>
2023-10-23 09:20:11 -0300

    Menu com design correto e outras melhorias do header em todas as paginas

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
             <a href="CLIENTE/view/carrinho.html"><ion-icon name="car-sport-outline"></ion-icon></a>
         </div>
         <div class="menuResponsivoIcon">
-            <ion-icon name="menu-outline"></ion-icon>
+            <ion-icon name="menu-outline" id="iconResponsivo"></ion-icon>
         </div>
         <div class="menuResponsivo display">
             <div class="principalResponsivo">
@@ -69,10 +69,13 @@
         let btn = document.querySelector('.menuResponsivoIcon');
         let menuMobile = document.querySelector('.menuResponsivo');
         var width = document.documentElement.clientWidth;
+        var iconResponsivo = document.getElementById('iconResponsivo');
 
         btn.addEventListener('click', () => {
             menuMobile.classList.toggle('display');
+            iconResponsivo.classList.toggle('white');
         });
+
 
     </script>
 
