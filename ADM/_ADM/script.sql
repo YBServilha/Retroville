f5f888dd4dfbe9a2767c1382c0af15d05c72a017
YBServilha <yanservilha@gmail.com>
2023-12-05 19:58:09 -0300

    Teste sql

--- ADM/_ADM/script.sql
+++ ADM/_ADM/script.sql
@@ -85,8 +85,8 @@ INSERT INTO produtos (
 ) VALUES (
     '76oDXv22', '05-12-2023 19:26:36', 'CHEVROLET', 'OPALA', 'Sedan',
     70000.00, '12V', 'Preto', '150.000', '1989',
-    'MANUAL - 4 Marchas', 8, 'Esta não é apenas uma máquina; é um pedaço da história automotiva que foi restaurado e reimaginado para atender aos padrões de hoje. O interior reflete o luxo da década de 80, combinando conforto e nostalgia de uma forma única. Cada detalhe foi cuidadosamente mantido para preservar a autenticidade deste clássico. 
-  Imagine-se dirigindo pelas estradas, as luzes noturnas dançando no exterior preto espelhado, enquanto você segura o volante clássico e sente o poder do motor respondendo ao seu toque. Este Opala Diplomata 1989 preto não é apenas um carro; é uma experiência que desperta os sentidos e evoca memórias de uma era dourada.', 'carro1.jpg', 'opala-inicio.png',
+    'MANUAL - 4 Marchas', 8, 'Esta n├úo ├® apenas uma m├íquina; ├® um peda├ºo da hist├│ria automotiva que foi restaurado e reimaginado para atender aos padr├Áes de hoje. O interior reflete o luxo da d├®cada de 80, combinando conforto e nostalgia de uma forma ├║nica. Cada detalhe foi cuidadosamente mantido para preservar a autenticidade deste cl├íssico.
+  Imagine-se dirigindo pelas estradas, as luzes noturnas dan├ºando no exterior preto espelhado, enquanto voc├¬ segura o volante cl├íssico e sente o poder do motor respondendo ao seu toque. Este Opala Diplomata 1989 preto n├úo ├® apenas um carro; ├® uma experi├¬ncia que desperta os sentidos e evoca mem├│rias de uma era dourada.', 'carro1.jpg', 'opala-inicio.png',
     'opala-historia.png', 'carro2.jpg', 'carro3.jpg', 'carro4.jpg', 'carro5.jpg',
     'carro6.jpg', 'carro7.jpg', 'carro8.jpg', 'carro9.jpg', '1'
 );
@@ -98,7 +98,7 @@ INSERT INTO produtos (
 ) VALUES (
     'xWMq7RQQ', '05-12-2023 19:49:55', 'CHEVROLET', 'D20', 'Caminhonete',
     1650000.00, '4.0', 'Vermelho', '12.000', '1993',
-    'MANUAL', 6, 'O Chevrolet D20 foi uma picape produzida no Brasil de 1985 a 1996. Conhecida por sua robustez e versatilidade, atendia tanto às necessidades de transporte de carga como ao uso pessoal. Oferecida com diversas opções de motores, a D20 ganhou destaque e é lembrada como um ícone da indústria automobilística brasileira.', 'carro1.png', 'd20inicio.png',
+    'MANUAL', 6, 'O Chevrolet D20 foi uma picape produzida no Brasil de 1985 a 1996. Conhecida por sua robustez e versatilidade, atendia tanto ├ás necessidades de transporte de carga como ao uso pessoal. Oferecida com diversas op├º├Áes de motores, a D20 ganhou destaque e ├® lembrada como um ├¡cone da ind├║stria automobil├¡stica brasileira.', 'carro1.png', 'd20inicio.png',
     'd20historia.png', 'carro2.png', 'carro3.png', 'carro4.png', 'carro5.png',
     'carro6.png', 'carro7.png', 'carro8.png', 'carro9.png', '1'
 );
